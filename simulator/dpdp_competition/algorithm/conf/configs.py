09cf65c0030841e6fe4ea907c9f25e352e5499f9
lijuchengHome <724661934@qq.com>
2021-06-23 22:55:05 +0800

    修改了目录

--- simulator/dpdp_competition/algorithm/conf/configs.py
+++ simulator/dpdp_competition/algorithm/conf/configs.py
@@ -4,12 +4,12 @@ import os
 class configs(object):
     root_folder_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     algorithm_folder_path = os.path.join(root_folder_path, "algorithm")
-    config_ini_path = os.path.join(algorithm_folder_path, "src/config/config.ini")
-    customer_info_path = os.path.join(algorithm_folder_path,
-                                      "data/dynamic_pickup_and_delivery_testdata/customer/customer_info.json")
+    algorithm_data_path = os.path.join(algorithm_folder_path, "data")
+    algorithm_data_customer_path = os.path.join(algorithm_data_path, "customer")
+    algorithm_data_route_map_path = os.path.join(algorithm_data_path, "route_map")
+    customer_info_path = os.path.join(algorithm_data_customer_path, "customer_info.json")
 
-    route_cost_map_path = os.path.join(algorithm_folder_path,
-                                      "data/dynamic_pickup_and_delivery_testdata/route_map/route_cost_map.json")
+    route_cost_map_path = os.path.join(algorithm_data_route_map_path, "route_cost_map.json")
     data_interaction_folder_path = os.path.join(algorithm_folder_path, "data_interaction")
 
     vehicle_info_path = os.path.join(data_interaction_folder_path, "vehicle_info.json")
@@ -22,7 +22,6 @@ class configs(object):
 
     date = "1900-01-01"
     time = "0:0:0"
-    route_cost_map_file = "../ data / dynamic_pickup_and_delivery_testdata / route_map / route_cost_map.json"
 
     time_2_go = 10
     static_process_time_on_customer = 1800
