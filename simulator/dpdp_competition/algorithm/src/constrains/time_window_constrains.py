89298bb55126a195021ad11b9ec5ed6d178bbe32
lijucheng809 <62863995+lijucheng809@users.noreply.github.com>
2021-11-02 22:32:31 +0800

    约束模块化

---
+++ simulator/dpdp_competition/algorithm/src/constrains/time_window_constrains.py
@@ -0,0 +1,10 @@
+from simulator.dpdp_competition.algorithm.src.constrains.constrains_base import ConstrainsBase
+
+
+class TimeWindowConstrains(ConstrainsBase):
+
+    def __init__(self):
+        pass
+
+    def isFeasible(self) -> bool:
+        pass
